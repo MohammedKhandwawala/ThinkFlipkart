d2705f37076c512178466e44df2c0b5c222aef37
mak_52 <ee16b117@ee.iitm.ac.in>
2020-05-01 21:04:44 +0530

    No clue whats happening

--- Maths/Numbers.c
+++ Maths/Numbers.c
@@ -3,7 +3,15 @@
 int add(int x, int y){
 	return x + y;
 }
+//This edit is in dev branch
+void myFunc(){
+	printf("I am in dev branch");
+}
 
+//This edit is in master branch
+void myFunc(){
+	printf("I am in master branch");
+}
 int mul(int x, int y){
 	return x*y;
 }
--- String/string.cpp
+++ String/string.cpp
@@ -6,6 +6,10 @@ void greetings(char hello[]){
 void toUpper(char a[]){
 	printf("Converted string to Upper Case");
 }
+//edit in remote
+void myFunc(){
+	printf("I am in remote");
+}
 void toTitle(char a[]){
 	print("Converted to caps");
 }	
